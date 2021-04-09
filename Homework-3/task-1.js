b19fd24130c83e3ab0fa4d88ce0939f6a27707a7
Alonekit <nikirk138@yandex.ru>
2021-04-09 18:07:32 +0800

    hw-3_fix

--- Homework-3/task-1.js
+++ Homework-3/task-1.js
@@ -1,5 +1,5 @@
 var simpleArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 var i = 0;
 while (i < simpleArr.length) {
-    console.log(simpleArr[i++]);
+    document.writeln(simpleArr[i++]);
 }
